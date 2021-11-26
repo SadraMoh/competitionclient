a737156e8ad24e5c6b466b62ea6dd1b32d498458
Lemi <Lemi@DESKTOP-SDD69VQ>
2021-11-26 12:32:32 +0330

    coin banner, cleanup

--- src/app/areas/home/index/index.component.ts
+++ src/app/areas/home/index/index.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.scss']
 })
-export class IndexComponent implements OnInit {
+export class IndexComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+    // apply sky background-image
+    document.body.classList.add('sky');
+  }
+
+  ngOnDestroy(): void {
+    // remove sky background-image
+    document.body.classList.remove('sky');
   }
 
 }
